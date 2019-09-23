0c8eab596259935362df637ac01920a781b1d2bb
kimmiesjensen <kimm0961@videndjurs.net>
2019-09-23 10:20:56 +0200

    Import logo to Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+import Logo from '../../moellerupgods.png';
 
 
 
 const Header = () => {
     return ( 
         <div>
+            <img src={Logo} alt="Moellerup Gods Logo"/>
             <Navbar />
         </div>
      );
